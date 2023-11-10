9308344fc7b71bb7f093763efbb8023f4c65011e
Nathan Barry <nathanbarry474@gmail.com>
2023-11-10 09:09:35 -0600

    added labels

--- main.go
+++ main.go
@@ -3,38 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	a := New(2.0)
-	b := New(-3.0)
-	c := New(10.0)
-	d := Add(Mul(a, b), c)
+	a := New(2.0, "a")
+	b := New(-3.0, "b")
+	c := New(10.0, "c")
+	e := Mul(a, b, "e")
+	d := Add(e, c, "d")
+	f := New(-2.0, "c")
+	L := Mul(d, f, "L")
 
-	d.DisplayGraph()
+	L.DisplayGraph()
 }
 
 type Value struct {
-	Data float64
-	prev []*Value
-	op   string
+	Data  float64
+	prev  []*Value
+	op    string
+	label string
 }
 
-func New(f float64) *Value {
-	return &Value{Data: f}
+func New(f float64, l string) *Value {
+	return &Value{Data: f, label: l}
 
 }
 
-func Add(a, b *Value) *Value {
+func Add(a, b *Value, l string) *Value {
 	return &Value{
-		Data: a.Data + b.Data,
-		prev: []*Value{a, b},
-		op:   "+",
+		Data:  a.Data + b.Data,
+		prev:  []*Value{a, b},
+		op:    "+",
+		label: l,
 	}
 }
 
-func Mul(a, b *Value) *Value {
+func Mul(a, b *Value, l string) *Value {
 	return &Value{
-		Data: a.Data * b.Data,
-		prev: []*Value{a, b},
-		op:   "*",
+		Data:  a.Data * b.Data,
+		prev:  []*Value{a, b},
+		op:    "*",
+		label: l,
 	}
 }
 
@@ -57,13 +63,13 @@ func (root *Value) DisplayGraph() {
 }
 
 func (v *Value) Display() {
-	prevVal := []float64{}
+	prevVal := []string{}
 	for _, pv := range v.prev {
-		prevVal = append(prevVal, pv.Data)
+		prevVal = append(prevVal, pv.label)
 	}
 	if v.op == "" && len(prevVal) == 0 {
-		fmt.Printf("%v { }\n", v.Data)
+		fmt.Printf("%v: %v\n", v.label, v.Data)
 	} else {
-		fmt.Printf("%v { %v %v }\n", v.Data, v.op, prevVal)
+		fmt.Printf("%v: %v { %v %v }\n", v.label, v.Data, v.op, prevVal)
 	}
 }
